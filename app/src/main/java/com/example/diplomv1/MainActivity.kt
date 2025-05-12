2c46a963f5c09f2f2ae565ed873e0d5ca8f41c8c
maksimus <maksimka09shitikov@gmail.com>
2025-05-13 00:12:54 +0700

    Phase 1. Login, autor, main (user part) ready. Next is child

--- app/src/main/java/com/example/diplomv1/MainActivity.kt
+++ app/src/main/java/com/example/diplomv1/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.diplomv1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.diplomv1.R
+import com.example.diplomv1.utils.EncryptedPrefs
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logoutButton = findViewById<Button>(R.id.buttonLogout)
+        logoutButton.setOnClickListener {
+            val prefs = EncryptedPrefs.getPrefs(this)
+            prefs.edit().remove("login").apply()
+
+            startActivity(Intent(this, com.example.diplomv1.presentation.login.LoginActivity::class.java))
+            finish()
+        }
     }
 }
