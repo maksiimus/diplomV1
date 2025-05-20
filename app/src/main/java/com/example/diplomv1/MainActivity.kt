c1e50efe0ac9fc535f62ef7b3c355deda1a19710
maksimus <maksimka09shitikov@gmail.com>
2025-05-20 18:07:02 +0700

    interface for Main

--- app/src/main/java/com/example/diplomv1/MainActivity.kt
+++ app/src/main/java/com/example/diplomv1/MainActivity.kt
@@ -8,8 +8,6 @@ import com.example.diplomv1.lms.*
 import com.example.diplomv1.presentation.child.ChildListActivity
 import com.example.diplomv1.presentation.login.LoginActivity
 import com.example.diplomv1.utils.EncryptedPrefs
-import android.widget.Toast
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,18 +26,6 @@ class MainActivity : AppCompatActivity() {
         val toChildrenButton = findViewById<Button>(R.id.buttonToChildren)
         toChildrenButton.setOnClickListener {
             startActivity(Intent(this, ChildListActivity::class.java))
-        }
-        val centile = CentileEvaluator.evaluateCentile(
-            gender = Gender.BOY,
-            type = MeasurementType.HEIGHT_AGE,
-            ageOrHeight = 24.5f,
-            value = 86.2f
-        )
-        if (centile != null) {
-            Toast.makeText(this, "Центиль роста: %.1f".format(centile), Toast.LENGTH_LONG).show()
-        }
-        else {
-            Toast.makeText(this, "null", Toast.LENGTH_LONG).show();
         }
     }
     override fun onDestroy() {
